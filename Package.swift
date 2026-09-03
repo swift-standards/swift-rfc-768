d507dc2475ab39a54ec3d731b0fbbe9e5e3cea1b
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-03 15:04:24 +0200

    Converge dependency locations to their canonical organizations

--- Package.swift
+++ Package.swift
@@ -1,23 +1,6 @@
 // swift-tools-version: 6.4
 import PackageDescription
 
-extension String {
-    static let rfc768 = "RFC 768"
-}
-
-extension Target.Dependency {
-    static let rfc768 = Self.target(name: .rfc768)
-    static let standards = Self.product(
-        name: "Standard Library Extensions",
-        package: "swift-standard-library-extensions"
-    )
-    static let incits41986 = Self.product(
-        name: "ASCII",
-        package: "swift-ascii"
-    )
-    static let rfc791 = Self.product(name: "RFC 791", package: "swift-rfc-791")
-}
-
 let package = Package(
     name: "swift-rfc-768",
     platforms: [
@@ -51,12 +34,12 @@ let package = Package(
     targets: [
         .target(
             name: "RFC 768",
-            dependencies: [.standards, .incits41986, .rfc791]
+            dependencies: [.product(name: "Standard Library Extensions", package: "swift-standard-library-extensions"), .product(name: "ASCII", package: "swift-ascii"), .product(name: "RFC 791", package: "swift-rfc-791")]
         ),
         .target(
             name: "RFC 768 Standard Library Integration",
             dependencies: [
-                "RFC 768",
+                .target(name: "RFC 768"),
                 .product(
                     name: "Byte Standard Library Integration",
                     package: "swift-byte"
@@ -66,23 +49,19 @@ let package = Package(
         .testTarget(
             name: "RFC 768 Tests",
             dependencies: [
-                "RFC 768"
+                .target(name: "RFC 768")
             ]
         ),
         .testTarget(
             name: "RFC 768 Standard Library Integration Tests",
             dependencies: [
-                "RFC 768",
-                "RFC 768 Standard Library Integration",
+                .target(name: "RFC 768"),
+                .target(name: "RFC 768 Standard Library Integration"),
             ]
         ),
     ],
     swiftLanguageModes: [.v6]
 )
-
-extension String {
-    var tests: Self { self + " Tests" }
-}
 
 for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
     let ecosystem: [SwiftSetting] = [
